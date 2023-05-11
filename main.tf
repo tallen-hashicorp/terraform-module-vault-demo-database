4bc752553cf95e8691120014a3601cc732f9e15b
Tyler Allen <tyler.allen@hashicorp.com>
2023-05-11 16:02:03 +0100

    Update

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "database_secret" {
   source  = "app.terraform.io/vault-adoption/vault-demo-database-secrets/module"
-  version = "0.0.3"
+  version = "0.0.4"
   namespace_path  = var.namespace_path
   db_name         = postgresql_database.database.name
   db_host         = var.db_host
